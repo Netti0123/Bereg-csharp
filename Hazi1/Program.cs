c1a66aae4a89f9cfe1e3336cbf664e652fb53501
Netti0123 <113418004+Netti0123@users.noreply.github.com>
2023-02-09 09:05:19 +0100

    Add files via upload

--- Hazi1/Program.cs
+++ Hazi1/Program.cs
@@ -10,6 +10,41 @@ namespace Hazi1
     {
         static void Main(string[] args)
         {
+            int x;
+            int y = 3;
+            int z = 4;
+            int a = 9;
+
+            Console.WriteLine("Adj meg egy számot: ");
+            x = int.Parse(Console.ReadLine());
+
+
+            if (x % y == 0)
+            {
+                Console.WriteLine("A szám osztható 3-mal!");
+            }
+            else
+            {
+                Console.WriteLine("A szám nem osztható 3-mal!");
+            }
+            if (x % z == 0)
+            {
+                Console.WriteLine("A szám osztható 4-mal!");
+            }
+            else
+            {
+                Console.WriteLine("A szám nem osztható 4-mal!");
+            }
+            if (x % a == 0)
+            {
+                Console.WriteLine("A szám osztható 9-mal!");
+            }
+            else
+            {
+                Console.WriteLine("A szám nem osztható 9-mal!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
