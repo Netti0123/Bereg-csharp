7014c85b28c9dfd31097939093ee5de10a68e297
Netti0123 <113418004+Netti0123@users.noreply.github.com>
2023-02-10 10:47:47 +0100

    Add files via upload

---
+++ teszt/teszt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool False = true;
+            try 
+            { 
+            Console.WriteLine("Adj egy szamot");
+            int a= int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("ez nem szam");
+                False= false;
+
+            }
+
+            
+
+            if (False)
+                Console.WriteLine("Igaz volt");
+            else
+            {
+                Console.WriteLine("nem volt igaz");
+                Console.WriteLine("es meg rossz is");
+            }Console.ReadKey();
+        }
+    }
+}
