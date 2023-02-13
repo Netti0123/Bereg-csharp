909bf4fdb5b9a53f5714808e1b6a7b8e5298ea66
Netti0123 <113418004+Netti0123@users.noreply.github.com>
2023-02-13 14:40:37 +0100

    Add files via upload

---
+++ h_15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h_15
+{
+    internal class Program
+    {
+        static double Szam(int osz)
+        {
+            Console.WriteLine(osz);
+            
+            return 0.0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg hany oldal:");
+            double ere=Szam(int.Parse(Console.ReadLine()));
+            
+            Console.ReadKey();
+        }
+    }
+}
