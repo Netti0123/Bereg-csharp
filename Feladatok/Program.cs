ab4e40c6455ae14877b6ad90e8b4e246da144cbe
Netti0123 <113418004+Netti0123@users.noreply.github.com>
2023-02-14 14:46:07 +0100

    Add files via upload

---
+++ Feladatok/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladatok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> szam = new List<int>();
+            for (int i = 0; i < 3; i++)
+            szam.Add(int.Parse(Console.ReadLine()));
+            Console.WriteLine(szam.Max());
+
+            Console.ReadKey();
+        }
+    }
+}
