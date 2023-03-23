475273d058b4f397c1628aba8b5f0dbe5855c106
Netti0123 <113418004+Netti0123@users.noreply.github.com>
2023-03-23 09:58:11 +0100

    Add files via upload

---
+++ cimletek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cimletek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            cimletekoop c1 = new cimletekoop();
+            Console.WriteLine("Irjon be egy szamot: ");
+            int pnz = Convert.ToInt32(Console.ReadLine());
+            c1.setcimlet(pnz);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cimletek/cimletekoop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cimletek
+{
+    internal class cimletekoop
+    {
+        private int cimlet;
+        static int[] penz = { 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000 };
+        
+        public cimletekoop(){ }
+
+        public cimletekoop(int cimlet)
+        {
+            this.cimlet = cimlet;
+        } 
+        public int getcimlet()
+        {
+            return this.getcimlet();
+        }
+
+        public void setcimlet(int cimlet)
+        {
+            this.cimlet = cimlet;
+            int val = 0;
+            for (int i = penz.Length-1; i >= 0; i--)
+            {
+                while (cimlet >= penz[i])
+                {
+                    cimlet -= penz[i];
+                    val++;
+                }
+            }        Console.WriteLine(val);
+        }
+    }
+
+}
